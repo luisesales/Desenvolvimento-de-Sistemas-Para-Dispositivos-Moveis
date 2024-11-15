92957d076215bd847b2dd323be50dc1b207ce506
luisesales <duduosales@hotmail.com>
2024-11-14 22:37:23 -0300

    updating mini _projeto_3 - adding addLugar untested

--- mini_projeto_3/lib/model/PaisesModel.dart
+++ mini_projeto_3/lib/model/PaisesModel.dart
@@ -6,20 +6,25 @@ import 'package:mini_projeto_3/data/dados.dart';
 class PaisesModel with ChangeNotifier {  
   
   List<Lugar> _lugaresFavoritos = [];
+  List<Lugar> _lugares = [];
   List<Pais> _paises = [];
 
   List<Lugar>  get favoritos => _lugaresFavoritos;
+  List<Lugar> get todos_lugares => _lugares;
   List<Pais> get todos_paises => _paises;    
 
   void initialize(){
     for(Pais p in paises){
       _paises.add(p);
     }    
+    for(Lugar l in lugares){
+      _lugares.add(l);
+    }
     notifyListeners();
   }
 
   List<Lugar> getLugares(String id){
-    return lugares.where((lugar) {return lugar.paises.contains(id);}).toList();
+    return _lugares.where((lugar) {return lugar.paises.contains(id);}).toList();
   }
   
   bool contains(Lugar lugar){
@@ -29,5 +34,15 @@ class PaisesModel with ChangeNotifier {
   void favorite(Lugar lugar){
    _lugaresFavoritos.contains(lugar) ? _lugaresFavoritos.remove(lugar) : _lugaresFavoritos.add(lugar);
    notifyListeners(); 
+  }
+
+  void addLugar(Lugar lugar){
+    _lugares.add(lugar);
+    notifyListeners();
+  }
+  bool removeLugar(Lugar lugar){
+    bool result =  _lugares.remove(lugar);
+    notifyListeners();
+    return result;
   }
 }
--- mini_projeto_3/lib/screens/adicionarLugar.dart
+++ mini_projeto_3/lib/screens/adicionarLugar.dart
@@ -17,8 +17,13 @@ class AdicionarLugar extends StatefulWidget {
 }
 
 class _AdicionarLugarState extends State<AdicionarLugar> {
+
+  
+
   @override
-  Widget build(BuildContext context) {    
+  Widget build(BuildContext context) {   
+    List<Lugar> todos_lugares = context.watch<PaisesModel>().todos_lugares;
+    List<Pais> todos_paises = context.watch<PaisesModel>().todos_paises;
     final _titleController = TextEditingController();
     String? _imageURL; 
     double _avaliation = 0.0; 
@@ -28,15 +33,59 @@ class _AdicionarLugarState extends State<AdicionarLugar> {
     bool _wrongTitle = false;
     bool _wrongPais = false;  
     bool _wrongImage = false;  
-    bool _wrongRecomendation = false;  
-    bool _wrongAvaliation = false;  
-    bool _wrongCost = false;  
+    bool _wrongRecomendation = false;      
 
     void _updateValue(dynamic newValue, dynamic variable){
       setState(() {
         variable = newValue;
       });
     }
+
+    void _saveUser(){
+    setState(() {
+      if(_titleController.text.isEmpty){
+        _wrongTitle = true;
+        return;
+      }
+      if(_imageURL!.isEmpty ||
+       (todos_lugares.any((lugar) => (lugar.imagemUrl == _imageURL))) 
+       ){
+        _wrongImage = true;
+        return;
+      } 
+      for(TextEditingController t in _recomendations){
+        if(t.text.isEmpty){
+          _wrongRecomendation = true;
+          return;
+        }
+      }
+      if(_countries.isEmpty){
+        _wrongPais = true;
+        return;
+      }
+      List<String> countriesIds = [];
+      for(String s in _countries){
+        String countryId = todos_paises.firstWhere((pais) => pais.titulo == s).id;
+        if(!countriesIds.contains(countryId)){
+          countriesIds.add(countryId);
+        }
+      }
+      List<String> formatRecomendations = [];
+      int index = 1;
+      for(TextEditingController t in _recomendations){
+        formatRecomendations.add("${index++}. ${t.text}");
+      }
+      Provider.of<PaisesModel>(context).addLugar(Lugar(
+        id: "p${todos_lugares.length+1}", 
+        paises: countriesIds, 
+        titulo: _titleController.text, 
+        imagemUrl: _imageURL, 
+        recomendacoes: formatRecomendations, 
+        avaliacao: _avaliation, 
+        custoMedio: _cost)
+      );
+    });    
+   }
 
     return Scaffold(
       appBar: AppBar(
@@ -47,91 +96,142 @@ class _AdicionarLugarState extends State<AdicionarLugar> {
         backgroundColor: ThemeData().primaryColor,
       ),
       drawer: MeuDrawer(),
-      body: SingleChildScrollView(child : Container(
-        padding: EdgeInsets.all(32),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            Container(
-              child: Text(
-                "Adicionar Lugar",
-                style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+      body: Stack(children: [ 
+        SingleChildScrollView(child : Container(
+          padding: EdgeInsets.all(32),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Container(
+                child: Text(
+                  "Adicionar Lugar",
+                  style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+                ),
+                margin: EdgeInsets.only(bottom: 16),
               ),
-              margin: EdgeInsets.only(bottom: 16),
-            ),
-            Form(
-              child: Column(
-                children: [
-                  TextFormField(
-                    decoration: InputDecoration(
-                      labelText: 'Título',
+              Form(
+                child: Column(
+                  children: [
+                    TextFormField(
+                      controller: _titleController,
+                      decoration: InputDecoration(
+                        labelText: 'Título',
+                      ),                  
                     ),
-                    validator: (value) {
-                      if (value == null || value.isEmpty) {
-                        return 'Por favor insira um título';
-                      }
-                      return null;
-                    },
-                  ),
-                  Center(
-                    child: Wrap(
-                      alignment: WrapAlignment.center,
-                      crossAxisAlignment: WrapCrossAlignment.center,
-                      spacing: 4.0,
-                      children: [
-                        Expanded(
-                          flex: 2,
-                          child: Expanded(
-                            child: DoubleNumberPicker(
-                              step: 0.1, 
-                              precision: 1,
-                              maxValue: 5,
-                              minValue: 0,
-                              labelText: '', 
-                              labelIcon: Icons.star,
-                              onChanged: (double){_updateValue(double,_avaliation);}
-                              )
-                          ),                                                    
+                    Visibility(
+                      visible: _wrongTitle, 
+                      child: Text(
+                          "Por favor insira um título",
+                          style: TextStyle(
+                            color: Colors.red[600],
+                            fontSize: 8,
                         ),
-                        Expanded(
-                          flex: 2,
-                          child: Expanded(
-                            child: DoubleNumberPicker(
-                              step: 5, 
-                              precision: 2,
-                              maxValue: double.infinity,
-                              minValue: 0,
-                              labelText: 'Custo :',
-                              labelIcon: Icons.attach_money,
-                              onChanged: (double){_updateValue(double,_cost);}
-                              )
-                          ),                                                    
-                        ), 
-                        Container(
-                          margin: EdgeInsets.only(top: 16, bottom: 16),
-                          child: imagePicker(onImagePicked: (String){_updateValue(String,_imageURL);},)   
-                        ),                           
-                        DynamicTextInput(
-                          title: "Recomendações",
-                          label : "Recomendação",                          
-                          controllers: _recomendations,
-                        ),
-                        DynamicDropdownList(
-                          title: "Países",
-                          label: "País",
-                          selectedList: _countries,
-                          list: context.watch<PaisesModel>().todos_paises.map((pais) => pais.titulo).toList(),    
-                        ),
-                      ],
+                      ),
+                    ),   
+                    Center(
+                      child: Wrap(
+                        alignment: WrapAlignment.center,
+                        crossAxisAlignment: WrapCrossAlignment.center,
+                        spacing: 4.0,
+                        children: [
+                          Expanded(
+                            flex: 2,
+                            child: Expanded(
+                              child: DoubleNumberPicker(
+                                step: 0.1, 
+                                precision: 1,
+                                maxValue: 5,
+                                minValue: 0,
+                                labelText: '', 
+                                labelIcon: Icons.star,
+                                onChanged: (double){_updateValue(double,_avaliation);}
+                                )
+                            ),                                                    
+                          ),
+                          Expanded(
+                            flex: 2,
+                            child: Expanded(
+                              child: DoubleNumberPicker(
+                                step: 5, 
+                                precision: 2,
+                                maxValue: double.infinity,
+                                minValue: 0,
+                                labelText: 'Custo :',
+                                labelIcon: Icons.attach_money,
+                                onChanged: (double){_updateValue(double,_cost);}
+                                )
+                            ),                                                    
+                          ), 
+                          Container(
+                            margin: EdgeInsets.only(top: 16, bottom: 16),
+                            child: imagePicker(onImagePicked: (String){_updateValue(String,_imageURL);},)   
+                          ), 
+                          Visibility(
+                            visible: _wrongImage, 
+                            child: Text(
+                                "Por favor insira uma imagem não registrada",
+                                style: TextStyle(
+                                  color: Colors.red[600],
+                                  fontSize: 8,
+                              ),
+                            ),
+                          ),                             
+                          DynamicTextInput(
+                            title: "Recomendações",
+                            label : "Recomendação",                          
+                            controllers: _recomendations,
+                          ),
+                          Visibility(
+                            visible: _wrongRecomendation, 
+                            child: Text(
+                                "Por favor insira uma recomendação",
+                                style: TextStyle(
+                                  color: Colors.red[600],
+                                  fontSize: 8,
+                              ),
+                            ),
+                          ),                             
+                          DynamicDropdownList(
+                            title: "Países",
+                            label: "País",
+                            selectedList: _countries,
+                            list: todos_paises.map((pais) => pais.titulo).toList(),    
+                          ),
+                          Visibility(
+                            visible: _wrongPais, 
+                            child: Text(
+                                "Por favor insira um país",
+                                style: TextStyle(
+                                  color: Colors.red[600],
+                                  fontSize: 8,
+                              ),
+                            ),
+                          ),                                
+                        ],
+                      ),
                     ),
-                  ),
-                  
-                ],
+                    
+                  ],
+                ),
               ),
-            ),
-          ]),
-        )
-      )
-    );
+            ]),
+          )
+        ),
+        Positioned( 
+          bottom: 16, 
+          right: 16, 
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              FloatingActionButton(
+                onPressed: _saveUser,                  
+                tooltip: 'Salvar Dados',
+
+                child: Icon(Icons.save),
+              ),
+          ],),
+        ),      
+      ]),
+    );  
   }
 }
